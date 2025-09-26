fbb099d9546b642bbe2181ccc2442789d5835b4f
George Weber <george.r.weber@nasa.gov>
2025-09-26 10:22:20 -0400

    Simplify Tensor.reorder with einsum

--- materialite/tensor.py
+++ materialite/tensor.py
@@ -469,12 +469,8 @@ class Tensor(ABC):
                 f"the original tensor's '{self.dims_str}'"
             )
 
-        permutation = [self.dims_str.index(dim) for dim in dims]
-
-        # Add component dimensions to the permutation
-        permutation.extend(range(len(self.dims_str), self.components.ndim))
-
-        new_components = np.transpose(self.components, permutation)
+        # Use einsum to reorder (ending ellipsis for component dimensions)
+        new_components = np.einsum(f"{self.dims_str}... -> {dims}...", self.components)
 
         return type(self)(new_components, dims)
 
@@ -1581,12 +1577,10 @@ class Orientation:
                 f"the original orientation's '{self.dims_str}'"
             )
 
-        permutation = [self.dims_str.index(dim) for dim in dims]
-
-        # Add component dimensions to the permutation
-        permutation.extend(range(len(self.dims_str), self.rotation_matrix.ndim))
-
-        new_components = np.transpose(self.rotation_matrix, permutation)
+        # Use einsum to reorder (ending ellipsis for component dimensions)
+        new_components = np.einsum(
+            f"{self.dims_str}... -> {dims}...", self.rotation_matrix
+        )
 
         return type(self)(new_components, dims)
 
